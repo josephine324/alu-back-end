015025550984cc7e579a8ac91874d596ee87941c
josephine324 <j.mutesi@alustudent.com>
2023-11-02 12:29:30 -0700

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+import sys
+import requests
+
+def fetch_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+
+    try:
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        user_data = user_response.json()
+
+        todos_response = requests.get(todos_url)
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
+
+        completed_tasks = [task for task in todos_data if task["completed"]]
+        total_tasks = len(todos_data)
+
+        print(f"Employee {user_data['name']} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+
+        for task in completed_tasks:
+            print(f"    {task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    fetch_employee_todo_progress(employee_id)
+
